89cdc59c155ef25c11242e3d1d8f602dbfa8484f
Mostafijur <mrkajol550@gmail.com>
2023-11-18 17:10:00 +0600

    Test: Mock and Spy

--- src/test/java/com/learn/junit/booking/services/BookingServiceTestMockVoidMethods.java
+++ src/test/java/com/learn/junit/booking/services/BookingServiceTestMockVoidMethods.java
@@ -2,20 +2,15 @@ package com.learn.junit.booking.services;
 
 import com.learn.junit.booking.model.BookingDAO;
 import com.learn.junit.booking.model.BookingRequest;
-import com.learn.junit.booking.utils.BusinessException;
 import com.learn.junit.booking.utils.MailSender;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyDouble;
 import static org.mockito.Mockito.*;
 
-class BookingServiceTestSpies {
+class BookingServiceTestMockVoidMethods {
 
 	private BookingService bookingService;
 	PaymentService paymentServiceMock;
@@ -33,34 +28,6 @@ class BookingServiceTestSpies {
 		bookingService = new BookingService(paymentServiceMock, roomServiceMock, bookingDAOMock, mailSenderMock);
 
 	}
-
-	/**
-	 *  Mock  = dummy object with no real logic
-	 * spy = partial mock
-	 *  Spy = real object with real logic that can be modified
-	 *
-	 */
-
-	@Test
-	void should_MakeBooking_When_InputOk() {
-		//given
-		BookingRequest bookingRequest = new BookingRequest("1", LocalDate.of(2023, 01, 01), LocalDate.of(2023, 01, 05), 2, true);
-
-		//when
-		String bookingId = bookingService.makeBooking(bookingRequest);
-
-		//then
-		verify(bookingDAOMock).save(bookingRequest);
-		System.out.println("Booking Id: " + bookingId);
-
-	}
-
-	/**
-	 *  mock =  when(mock.method()).thenReturn();
-	 *
-	 *  spy = doReturn().when(spy).method();
-	 *
-	 */
 
 	@Test
 	void should_CancelBooking_When_InputOk() {
